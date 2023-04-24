ab6c6417f27d100d2a98c028d7ca2977c0fa2bf7
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-04-23 22:00:57 -0300

    Visual structure - Creating an accessible modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import logoImg from "../../assets/logo.svg";
+import * as Dialog from '@radix-ui/react-dialog';
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
@@ -8,7 +9,21 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
