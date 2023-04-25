66bacffb12cf929d8496f90b6e6a953b2d7efbc2
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-04-24 21:39:50 -0300

    API connection - Performing HTTP request  (version async await)

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -10,12 +10,15 @@ import {
 
 export function Transactions() {
   useEffect(() => {
-    fetch('http://localhost:3333/transactions')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        
-      })
+    async function loadTransactions() {
+      const response = await fetch('http://localhost:3333/transactions')
+      const data = await response.json()
+
+      console.log(data);
+      
+    }
+
+    loadTransactions()
   }, [])
 
   return (
