6ba7a2c740bba461006be3061360bd81a29c1d2e
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-04-24 21:36:18 -0300

    API connection - Performing HTTP request

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./SearchForm";
@@ -8,6 +9,15 @@ import {
 } from "./styles";
 
 export function Transactions() {
+  useEffect(() => {
+    fetch('http://localhost:3333/transactions')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        
+      })
+  }, [])
+
   return (
     <div>
       <Header />
